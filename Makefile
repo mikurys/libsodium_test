8b0927e895582911e5228d2a2ecd7c3b38b20a82
Rysiek <mikurys@gmail.com>
2017-05-29 15:38:30 +0200

    aligment test

--- Makefile
+++ Makefile
@@ -1,12 +1,12 @@
-PROGRAM_OPTION=--auth #posibilities: --auth, --encrypt-auth
-MIN_SIZE=64024 #minimum data to calculate avg crypto speed, default value 1024 (increase to get better data)
-QUALITY=128 #chart quality, default value 4 (increase to get better chart)
+PROGRAM_OPTION=--encrypt #posibilities: --auth, --encrypt-auth, --encrypt
+MIN_SIZE=655350 #minimum data to calculate avg crypto speed, default value 1024 (increase to get better data)
+QUALITY=64 #chart quality, default value 4 (increase to get better chart)
 all:
 	g++ -std=c++14 -lsodium -o libsodium_test main.cpp
 run:
 	g++ -std=c++14 -lsodium -o libsodium_test main.cpp
 	./libsodium_test
 test:
-	g++ -std=c++14 -lsodium -O3 -o libsodium_test main.cpp
+	g++ -std=c++14 -lsodium -O3 -march=native -o libsodium_test main.cpp
 	time ./libsodium_test $(MIN_SIZE) $(QUALITY) $(PROGRAM_OPTION) > result.txt
 	gnuplot gplot_skrypt
--- main.cpp
+++ main.cpp
@@ -4,7 +4,8 @@
 #include <cmath>
 
 //constexpr size_t SIZE = 32*1024*1024;
-constexpr size_t SIZE = 64*1024;
+constexpr size_t SIZE = 2*1024;
+constexpr size_t MAX_ALLIGMENT = 256;
 constexpr size_t ciphertext_len = crypto_secretbox_MACBYTES * SIZE + SIZE;
 
 typedef std::chrono::steady_clock Time;
@@ -15,12 +16,15 @@ typedef std::chrono::duration<double> fsec;
 std::string program_option("--encrypt-auth");
 std::string encrytp_auth("--encrypt-auth");
 std::string auth("--auth");
+std::string encryption("--encrypt");
 
 unsigned char nonce[crypto_secretbox_NONCEBYTES];
 unsigned char key[crypto_secretbox_KEYBYTES];
 unsigned char *buff = new unsigned char[SIZE];
-unsigned char *copy_buff = new unsigned char[SIZE];
+unsigned char *copy_buff = new unsigned char[SIZE+MAX_ALLIGMENT];
 unsigned char *ciphertext = new unsigned char[ciphertext_len];
+
+bool is_buffer_copied = false;
 
 void init(){
 	randombytes_buf(nonce, sizeof nonce);
@@ -28,18 +32,21 @@ void init(){
 	randombytes_buf(buff, SIZE);
 }
 
-size_t encrypt(unsigned char *ciphertext, unsigned char *buff, size_t size, size_t sizeB){
+size_t encrypt(unsigned char *ciphertext, unsigned char *buff, size_t size, size_t sizeB, size_t alligment){
 	int number_of_parts = static_cast<size_t>(std::floor(static_cast<double>(size)/sizeB));
 	size_t true_size = number_of_parts * sizeB;
 	unsigned char *buff_ptr = buff;
 	unsigned char *ciphertext_ptr = ciphertext;
 	while(buff_ptr+sizeB <= buff+size)
 	{
-		std::copy(buff_ptr, buff_ptr+sizeB, copy_buff);
+		//if(is_buffer_copied)
+		std::copy(buff_ptr, buff_ptr+sizeB, copy_buff+alligment);
 		if(program_option == encrytp_auth)
-			crypto_secretbox_easy(ciphertext_ptr, copy_buff, sizeB, nonce, key);
+			crypto_secretbox_easy(ciphertext_ptr, copy_buff+alligment, sizeB, nonce, key);
 		else if(program_option == auth)
-			crypto_onetimeauth(ciphertext+sizeB, copy_buff, sizeB, key);
+			crypto_onetimeauth(ciphertext_ptr, copy_buff+alligment, sizeB, key);
+		else if(program_option == encryption)
+			crypto_stream_xor(ciphertext_ptr, copy_buff+alligment, sizeB, nonce, key);
 		buff_ptr += sizeB;
 		ciphertext_ptr += sizeB + crypto_secretbox_MACBYTES;
 	}
@@ -55,64 +62,52 @@ int main(int argc, char **argv)
 		min_size = atoi(argv[1]);
 		quality = atoi(argv[2]);
 		program_option = std::string(argv[3]);
+		//if(std::string(argv[4])==std::string("--copy")) is_buffer_copied=true;
 	}
 
 	if(program_option == encrytp_auth)
-		std::cout << "Encrypt+Authorize, without buffer copy, Mega bit/sec, MIN_SIZE=" << min_size << ", QUALITY=" << quality << std::endl;
+		std::cout << "Encrypt+Authorize, Mega bit/sec, MIN_SIZE=" << min_size << ", QUALITY=" << quality << std::endl;
 	else if(program_option == auth)
-		std::cout << "Authorize, without buffer copy, Mega bit/sec, MIN_SIZE=" << min_size << ", QUALITY=" << quality << std::endl;
+		std::cout << "Authorize, Mega bit/sec, MIN_SIZE=" << min_size << ", QUALITY=" << quality << std::endl;
+	else if(program_option == encryption)
+		std::cout << "Encryption, Mega bit/sec, MIN_SIZE=" << min_size << ", QUALITY=" << quality << std::endl;
 
 	if (sodium_init() == -1) {
 		return 1;
 	}
 
 	init();
-	size_t size=16;
-	double dbl_size=static_cast<double>(size), multipler = std::pow(2, 1.0/quality);
-	for ( ; size<=SIZE; size=std::round(dbl_size))
+	for (size_t alligment=0; alligment<=MAX_ALLIGMENT; alligment++)
+	//size_t size=16;
+	//double dbl_size=static_cast<double>(size), multipler = std::pow(2, 1.0/quality);
+	//for ( ; size<=SIZE; size=std::round(dbl_size))
 	{
-	//	if(size > 2000 && size%500 != 0) continue;
-	//	else if(size > 9000 && size%1000 != 0) continue;
-	//	else if(size > 100000 && size%100000 != 0) continue;
-	//	else if(size > 1000000 && size%1000000 != 0) continue;
-	//	else if(size > 10000000 && size%10000000 != 0) continue;
-		//unsigned char *buff = new unsigned char[size];
-		//randombytes_buf(buff, size);
-		size_t sizeB=8;
-		double dbl_sizeB=static_cast<double>(sizeB);
-		for (; sizeB<=size; sizeB=std::round(dbl_sizeB))
+		size_t size=16;
+		double dbl_size=static_cast<double>(size), multipler = std::pow(2, 1.0/quality);
+		for ( ; size<=SIZE; size=std::round(dbl_size))
+		//for (size_t alligment=0; alligment<=MAX_ALLIGMENT; alligment++)
 		{
-		//	if(sizeB > 256 && sizeB%16 != 0) continue;
-		//	else if(sizeB > 2048 && sizeB%128 != 0) continue;
-		//	else if(sizeB > 16*1024 && sizeB%2048 != 0) continue;
-		//	else if(sizeB > 256*1024 && sizeB%(256*1024) != 0) continue;
-		//	else if(sizeB > 2*1024*1024 && sizeB%(2*1024*1024) != 0) continue;
-		//	else if(sizeB > 32*1024*1024 && sizeB%(32*1024*1024) != 0) continue;
-		//	else if(sizeB > 512*1024*1024 && sizeB%(512*1024*1024) != 0) continue;
-
 			size_t true_size;
 			auto start_time = Time::now();
 			size_t iter = std::ceil(static_cast<double>(min_size)/(size));
 			for (size_t i=0; i<iter; i++)
 			{
-			  	true_size = encrypt(ciphertext, buff, size, sizeB);
+			  	true_size = encrypt(ciphertext, buff, size, size, alligment);
 			}
 			auto end_time = Time::now();
 
 			fsec time = end_time - start_time;
-			//ms m = std::chrono::duration_cast<ms>(time);
 			ns n = std::chrono::duration_cast<ns>(time);
-			//std::cout << "Buffer size: " << size << " crypto_secretbox_easy time in ns: " << m.count()	<< std::endl;
-			std::cout << size << " " << sizeB << " " << (static_cast<double>(true_size)*1000*1000*1000)/(static_cast<double>(n.count())*1024*1024)*8*iter	<< std::endl;
-
-			//std::cerr << std::endl << "iter=" << iter << " size=" << size << " SIZE=" << SIZE << " n.count=" << n.count() << std::endl;
-			dbl_sizeB*=multipler;
+			std::cout << size << " " << alligment << " " << (static_cast<double>(true_size)*1000*1000*1000)/(static_cast<double>(n.count())*1024*1024)*8*iter	<< std::endl;
+			dbl_size*=multipler;
 		}
 		std::cout << std::endl;
-		std::cerr << "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b" << size << '/' << SIZE;
-		dbl_size*=multipler;
+		//std::cerr << "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b" << size << '/' << SIZE;
+		//dbl_size*=multipler;
+		std::cerr << "\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b\b" << alligment << '/' << MAX_ALLIGMENT;
 	}
 	delete[] buff;
+	delete[] copy_buff;
 	delete[] ciphertext;
 	return 0;
 }
